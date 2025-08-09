342cfeeeef816232b3a451e551ccb8853471855e
Craig Idaeho <idacraig90@gmail.com>
2025-08-09 03:10:11 -0400

    Create an object to represent a studentd

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 
 
 console.log("Lab 8 Web structure is ready!");
+
+
+
+//Part 1: Understanding and Creating Objects
+let student = {
+    name: "Mike",
+    age: 25,
+    enrolled: true,
+    courses: ["Math", "Computer", "English"],  
+    basicStudentInfo: function() {
+        // I had issues with this part as it wasn't in the class example. 
+        return `Student: ${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}`;
+        
+        // console.log(`Student: ${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}`); = I previously did it this way and returned "undifined"
+    }
+};
+
+console.log(student.name); // Mike
+console.log(student.age);  // 25
+console.log("Student Info:", student.basicStudentInfo()); // Student Info: Student: Mike, Age: 25, Enrolled: true
